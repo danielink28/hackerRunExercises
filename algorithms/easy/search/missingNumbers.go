08c3cbbc4becee2ca69ce4139bcf15011c0845b4
Daniel Saavedra <danielink28@gmail.com>
2020-09-28 20:28:32 -0500

    missing number, se crea tabla hash que tenga los valores y  luego comparo las tablas

--- algorithms/easy/search/missingNumbers.go
+++ algorithms/easy/search/missingNumbers.go
@@ -4,27 +4,44 @@ import (
 	"fmt"
 	"sort"
 )
+
 //clave valor tabla hash
 
 func main() {
-	values1 := []int32{203, 204, 205, 206, 207, 208, 203, 204, 205, 206}
-	values2 := []int32{203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204}
+	//values1 := []int32{203, 204, 205, 206, 207, 208, 203, 204, 205, 206}
+	//values2 := []int32{203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204}
+	values1 := []int32{11, 4, 11, 7, 13, 4, 12, 11, 10, 14}
+	values2 := []int32{11, 4, 11, 7, 3, 7, 10, 13, 4, 8, 12, 11, 10, 14, 12}
+
 	numbers := missingNumbers(values1, values2)
 	fmt.Println(numbers)
 }
 
-func missingNumbers(arr []int32, brr []int32) []int32 {
-	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
-	sort.Slice(brr, func(i, j int) bool { return brr[i] < brr[j] })
+func missingNumbers(arr []int32, brr []int32) (result []int32) {
+	var table = make(map[int32]int)
+	var table2 = make(map[int32]int)
 
-	//arr = deleteRepeated(arr)
-	//brr = deleteRepeated(arr)
-
-	if len(arr) > len(brr) {
-		return validate(arr, brr)
+	for _, value := range arr {
+		table[value] = table[value] + 1
 	}
-	return validate(brr, arr)
-	return nil
+	for _, value := range brr {
+		table2[value] = table2[value] + 1
+	}
+	if len(arr) > len(brr) {
+		for valor := range table {
+			if table[valor] != table2[valor] {
+				result = append(result, valor)
+			}
+		}
+	} else {
+		for valor := range table2 {
+			if table[valor] != table2[valor] {
+				result = append(result, valor)
+			}
+		}
+	}
+	sort.Slice(result, func(i, j int) bool { return result[i] < result[j] })
+	return result
 }
 
 func deleteRepeated(arr []int32) []int32 {
@@ -48,6 +65,7 @@ func Filter(arr []int32, cond func(int32, []int32) bool) (result []int32) {
 	return result
 }
 
+/*
 func validate(arr1 []int32, brr2 []int32) (result []int32) {
 	for _, val1 := range arr1 {
 		var lost = true
@@ -64,3 +82,4 @@ func validate(arr1 []int32, brr2 []int32) (result []int32) {
 	}
 	return result
 }
+*/
